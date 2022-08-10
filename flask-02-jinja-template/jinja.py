af2d88d4652ee290074bbae0d82882a1cacd24fb
mfevzidortbudak <mfevzidortbudak@gmail.com>
2022-08-10 22:42:23 +0300

    flask-01 and flask 02 added

--- flask-02-jinja-template/jinja.py
+++ flask-02-jinja-template/jinja.py
@@ -8,7 +8,10 @@ def head():
     return render_template('index.html', number1 = 7000, number2 = 9000)
 
 
-
+@app.route('/mult')
+def number():
+    var1, var2 = 5210, 8960
+    return render_template('body.html', num1 = var1, num2 = var2, multiplication = var1*var2)
 
 
 
